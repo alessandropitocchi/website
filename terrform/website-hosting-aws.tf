b5424d06c11e2a5e6a1f69ced4d542faab4ed776
alessandropitocchi <alessandropitocchiap@gmail.com>
2025-03-01 17:59:48 +0100

    created a Terraform configuration to provision an S3 bucket and CloudFront for hosting the website

---
+++ terrform/website-hosting-aws.tf
@@ -0,0 +1,184 @@
+variable "aws_region" {
+  default = "eu-west-3"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "alessandropitocchi.it"
+
+  tags = {
+    Name        = "alessandropitocchi.it"
+    Environment = "Prod"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "website_bucket_ownership" {
+  bucket = aws_s3_bucket.website_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "website_bucket" {
+  bucket                  = aws_s3_bucket.website_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_policy" "website_bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = {
+          AWS = aws_cloudfront_origin_access_identity.website_oai.iam_arn
+        },
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.website_bucket.id
+  key          = "index.html"
+  source       = "../index.html"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "css" {
+  bucket       = aws_s3_bucket.website_bucket.id
+  key          = "style.css"
+  source       = "../css/style.css"
+  content_type = "text/css"
+}
+
+resource "aws_cloudfront_origin_access_identity" "website_oai" {
+  comment = "OAI for alessandropitocchi.it"
+}
+
+resource "aws_cloudfront_distribution" "website_cdn" {
+  origin {
+    domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
+    origin_id   = "s3-origin"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.website_oai.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "CDN for alessandropitocchi.it"
+  default_root_object = "index.html"
+
+  aliases = [
+    "alessandropitocchi.it",
+    "www.alessandropitocchi.it"
+  ]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3-origin"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate.website_certificate.arn
+    ssl_support_method  = "sni-only"
+  }
+
+  tags = {
+    Name        = "alessandropitocchi.it"
+    Environment = "Prod"
+  }
+}
+
+data "aws_route53_zone" "main" {
+  name = "alessandropitocchi.it."
+}
+
+resource "aws_route53_record" "website" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "alessandropitocchi.it"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.website_cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.website_cdn.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_acm_certificate" "website_certificate" {
+  domain_name       = "alessandropitocchi.it"
+  validation_method = "DNS"
+  subject_alternative_names = [
+    "www.alessandropitocchi.it"
+  ]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name        = "alessandropitocchi.it"
+    Environment = "Prod"
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.website_certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 300
+}
+
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn         = aws_acm_certificate.website_certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+  depends_on              = [aws_route53_record.cert_validation]
+}
+
+output "cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.website_cdn.domain_name
+}
